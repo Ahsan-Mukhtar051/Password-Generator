9f13a5c18af10c3b63ff83ceff4c74a042ab7e63
Ahsan Mukhtar <81924304+Ahsan-Mukhtar051@users.noreply.github.com>
2024-12-16 18:03:58 +0500

    Add files via upload
    Build an app with HTML, CSS, JavaScript

---
+++ Passwords Generator/index.js
@@ -0,0 +1,17 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+    "/"];
+let password1Element = document.getElementById("password1");
+let password2Element = document.getElementById("password2");
+
+function generatePassword() {
+    let password1 = "";
+    let password2 = "";
+    for(let i= 0; i<15; i++) {
+        password1 += characters[Math.floor(Math.random() * characters.length)];
+        password2 += characters[Math.floor(Math.random() * characters.length)];
+    }
+    password1Element.textContent = password1;
+    password2Element.textContent = password2;
+}
+
+
